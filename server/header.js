c2385ed0b91a3af858cc9a62dc2da032b045ef2f
Dmitriy Samortsev <samortsev@gmail.com>
2017-04-09 15:58:49 +0300

    comment

--- server/header.js
+++
@@ -1,7 +0,0 @@
-﻿/*
-	Copyright (C) 2011-2016 Samortsev Dmitry (samortsev@gmail.com). All Rights Reserved.
-*/
-exports.Objectum = function (config) {
-if (config) {
-	config = config.config || config;
-};
